eed7a4d9212b623b37dac07a3eae734ff8784cd8
Ashish Kumar <ashishyadavupc@gmail.com>
2025-04-26 00:53:43 +0530

    Final Deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello, ECS!"})
+    return jsonify({"message": "Welcome to ECS [EC2] deployment"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
